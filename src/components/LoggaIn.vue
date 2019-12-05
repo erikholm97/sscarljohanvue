c39ccbd47554188ad3da268a8297d25488445015
erikholm97 <erikholm97@hotmail.com>
2019-12-05 20:06:01 +0100

    Fixed broken input

--- src/components/LoggaIn.vue
+++ src/components/LoggaIn.vue
@@ -1,61 +1,80 @@
 <template>
-    <div >
-        <div id="login">
+    <div>
         <h1>Logga in</h1>
-        <input type="text" name="username" placeholder="Användarnamn" v-model="input.username" />
-        <input
-            type="password"
-            name="password"
-            v-model="input.password"
-            placeholder="Password"
-        />
-        <button type="button" v-on:click="login()">Login</button>
-        
-        <div><p id="login-message"></p></div>
+        <div class="login">
+            <input
+                type="text"
+                name="username"
+                placeholder="Användarnamn"
+                v-model="input.username"
+            />
+            <input
+                type="password"
+                name="password"
+                v-model="input.password"
+                placeholder="Password"
+            />
+            <button type="button" v-on:click="login()">Login</button>
+
+            <div><p id="login-message"></p></div>
         </div>
     </div>
-    
 </template>
 
 <script>
 export default {
-        name: "Login",
-        data(){
-            return{
+    name: "Login",
+    data() {
+        return {
+            input: {
+                username: "",
+                password: ""
+            }
+        };
+    },
+    methods: {
+        login() {
+            // Todo slå mot databas. Förhindra otillåtna bokstäver.
 
-                input:{
-                    username: "",
-                    password: ""
-                }
+            if (
+                this.input.username == "admin" &&
+                this.input.password == "admin"
+            ) {
+                document.getElementById("login-message").innerHTML =
+                    "Du lyckades"; // Byter ut p elementet mot denna text.
+            } else if (
+                this.input.username != "admin" &&
+                this.input.password == "admin"
+            ) {
+                document.getElementById("login-message").innerHTML =
+                    "Fel användarnamn";
+            } else if (
+                this.input.username == "admin" &&
+                this.input.password != "admin"
+            ) {
+                document.getElementById("login-message").innerHTML =
+                    "Fel lösenord";
+            } else {
+                document.getElementById("login-message").innerHTML =
+                    "Användaren existerar inte";
             }
-        },
-        methods:{
-            login(){ // Todo slå mot databas. Förhindra otillåtna bokstäver. 
-            
-            if(this.input.username == "admin" && this.input.password == "admin"){
-                document.getElementById("login-message").innerHTML = "Du lyckades"; // Byter ut p elementet mot denna text. 
-            }
-            else if(this.input.username != "admin" && this.input.password  == "admin"){
-                document.getElementById("login-message").innerHTML = "Fel användarnamn";
-            }
-            else if(this.input.username == "admin" && this.input.password != "admin"){
-                document.getElementById("login-message").innerHTML = "Fel lösenord";
-            }
-            else{
-                document.getElementById("login-message").innerHTML = "Användaren existerar inte";
-            }
-            
-            
-
         }
     }
-}
+};
 </script>
 
 <style scoped lang="scss">
-.login{
-    display: flex !important;
-    justify-content: center !important;
-    align-items: center !important;
+.login {
+    display: flex;
+    justify-content: center;
 }
+@media only screen and (max-width: 447px) { // Annars försvinner login knappen
+  .login {
+    display: flex;
+    justify-content: center;
+    
+    
+}
+}
+
 </style>
