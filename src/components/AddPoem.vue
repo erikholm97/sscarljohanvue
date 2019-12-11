b9cbcd3a0d690300439ff2cb33c42c33cf614649
jonhol18 <jonathanholen96@gmail.com>
2019-12-11 11:37:48 +0100

    Fungerande Firebase connection som visar förinlagd data i Firestore

--- src/components/AddPoem.vue
+++ src/components/AddPoem.vue
@@ -1,11 +1,41 @@
 <template>
-    <h1> Test </h1>
+    <div id="AddPoem">
+        <ul class="collection with-header">
+            <li class="collection-header">
+                <h4>Poems</h4>
+            </li>
+            <li v-for="Poems in poems" v-bind:key="Poems.id" class="collection-item">
+                {{Poems.Rubrik}}
+                {{Poems.PoemCont}}
+
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script>
+    import db from './firebaseInit'
     export default {
-        name: "AddPoem"
-    };
+        name: "AddPoem",
+        data(){
+            return{
+                poems: []
+            }
+        },
+        created() {
+            db.collection('Poems').get().then(querySnapshot => {
+                querySnapshot.forEach(doc => {
+                    const data = {
+                        'id' : doc.id,              //Firebase given id to 'poem'
+                        'Poem_Id' : doc.data().Poem_Id,         //Our given Id to the poem
+                        'Rubrik' : doc.data().Rubrik,           //The "rubrik" to the poem
+                        'PoemCont':doc.data().PoemCont          //The content of the poem, this is where the actually poem is stored.
+                    }
+                    this.poems.push(data)                   //This sends the data from firebase to div AddPoem
+                })
+            })
+        }
+    }
 </script>
 
 <style scoped>
