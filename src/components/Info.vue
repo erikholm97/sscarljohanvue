b29ab3bcf65c48b6464d6651920ebd66d07a9872
erikholm97 <erikholm97@hotmail.com>
2019-12-16 16:49:25 +0100

    lint and info page css

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -1,5 +1,43 @@
 <template>
     <div class="page-content">
+        <div class="side-block-img">
+            <h1>Fakta om ångbåten/projektet</h1>
+            <img
+                src="../assets/InfoImg.jpg"
+                class="carl-johan-img"
+                alt="steamboat picture"
+            />
+        </div>
+        <div class="side-block-text">
+            <ul>
+                <li>
+                    <p><strong>Längd:</strong> 85 fot</p>
+                </li>
+                <li>
+                    <p><strong>Bredd: </strong> 17 fot</p>
+                </li>
+                <li>
+                    <p><strong>Djupgående: </strong> 6 fot</p>
+                </li>
+                <li>
+                    <p>
+                        <strong>Maskinen: </strong> Maskinen är en
+                        compoundmaskin (med strålkondensor) på cirka 35-40 hp,
+                        55 ind hp.
+                    </p>
+                </li>
+                <li>
+                    <p>
+                        <strong>Fartyget är byggt av:</strong>
+                        Motala verkstads ingenjörer och skeppsstrongyggmästare,
+                        även hantverkare från strongygden runt Sommen.
+                    </p>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <!--
+    <div class="page-content">
         <div class="info-text">
             <h1 class="facts-header">Fakta om ångbåten/projektet</h1>
             <img
@@ -35,8 +73,7 @@
                     </li>
                 </ul>
             </div>
-        </div>
-    </div>
+        </div>-->
 </template>
 
 <script>
@@ -48,34 +85,43 @@ export default {
 
 <!-- Add "scoped" attristrongute to limit CSS to this component only -->
 <style scoped lang="scss">
-.info-text {
-    .facts-header {
-        text-align: center;
+.page-content {
+    display: flex;
+    align-items: center;
+    animation: animation 3s alternate;
+    h1 {
+        text-align: left;
     }
-    li {
-        display: flex;
-        align-items: center;
-        text-align: center;
-        margin: 0px;
+    .carl-johan-img {
+        align-items: left;
+        padding: 0px;
     }
-    @media only screen and (max-width: 0px),
-        only screen and (max-width: 1200px) {
-        .page-content {
-            display: flex;
-            align-items: center;
-            .info-text {
-                .carl-johan-img {
-                    padding: 0 !important;
-                    width: 100%;
-                    height: auto;
-                }
-            }
-            .side-block-text {
-                padding: 30px;
-            }
-            .side-block-text {
-                text-align: center;
-            }
+    .side-block-text {
+        padding: 30px;
+    }
+    ul {
+        list-style-type: none;
+    }
+    p {
+        letter-spacing: normal;
+    }
+}
+@keyframes animation {
+    0% {
+        opacity: 0;
+    }
+    100% {
+        opacity: 1;
+    }
+}
+@media only screen and (max-width: 0px), only screen and (max-width: 1200px) {
+    .page-content {
+        display: block;
+
+        .carl-johan-img {
+            padding: 0 !important;
+            width: 100%;
+            height: auto;
         }
     }
 }
