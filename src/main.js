ba55c5b6a497a6db8b5799b22b1a3367e67165b1
delil97 <delil.demirsoy@hotmail.com>
2019-12-26 23:03:24 +0100

    test

--- src/main.js
+++ src/main.js
@@ -3,6 +3,15 @@ import App from "./App.vue";
 import router from "./router.js";
 import { firestorePlugin } from "vuefire";
 
+require("dotenv").config();
+
+import * as VueGoogleMaps from "vue2-google-maps";
+Vue.use(VueGoogleMaps, {
+  load: {
+       key: "VUE_APP_GOOGLE_MAPS_API_KEY=ABcdEfGhIjklmNOpqrsTUvWXyzAbcD1EfGhiJK"
+}
+});
+ 
 Vue.use(firestorePlugin);
 Vue.config.productionTip = false;
 
