44a9628241451146fc7450db02876ab54fe9914a
Erik Holm <erikholm97@hotmail.com>
2019-12-27 01:12:12 +0100

    lng fix

--- src/components/HittaHit.vue
+++ src/components/HittaHit.vue
@@ -1,37 +1,29 @@
 <template>
-
-  <GmapMap :center="center" :map-type-id="mapTypeId" :zoom="15">
-      <GmapMarker
-        v-for="(item, index) in markers"
-        :key="index"
-        :position="item.position"
-        @click="center = item.position"
-      />
+    <GmapMap :center="center" :map-type-id="mapTypeId" :zoom="15">
+        <GmapMarker
+            v-for="(item, index) in markers"
+            :key="index"
+            :position="item.position"
+            @click="center = item.position"
+        />
     </GmapMap>
 </template>
 
 <script>
 export default {
-    
-  data() {
-    return {
-      center: { lat: 58.067630, lng: 15.018338  },
-      mapTypeId: "terrain",
-      markers: [
-          
-        { position: { lat:  58.067630, lng: 15.018338 } },
-      ]
-    };
-  }
+    data() {
+        return {
+            center: { lat: 58.068506, lng: 15.018399 },
+            mapTypeId: "terrain",
+            markers: [{ position: { lat: 58.06763, lng: 15.018338 } }]
+        };
+    }
 };
-
 </script>
-
-
 
 <style lang="scss" scoped>
 .vue-map-container {
-  height: 450px;
-  width: 100%;
+    height: 450px;
+    width: 100%;
 }
 </style>
